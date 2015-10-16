bc2bb61b1945575491e05fc5fe2fc6ae5c6d3886
zhaolu <zhaolu@jiuan.com>
2015-10-16 09:58:52 +0800

    通过log可以显示手机所在位置的经纬度

---
+++ readme.txt
@@ -0,0 +1,3 @@
+两个问题：
+1、记住要向高德申请KEY
+2、在AndroidManifest.xml中添加相应的权限
--- src/com/example/android_map_location/MainActivity.java
+++ src/com/example/android_map_location/MainActivity.java
@@ -1,12 +1,72 @@
 package com.example.android_map_location;
+
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.location.LocationManagerProxy;
+import com.amap.api.location.LocationProviderProxy;
+
 import android.app.Activity;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements AMapLocationListener{
+	private LocationManagerProxy mLocationManagerProxy;
+	private String TAG = "MainActivity";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		init();
+	}
+	
+	/**
+	 * 初始化定位
+	 */
+	private void init() {
+		// 初始化定位，只采用网络定位
+		mLocationManagerProxy = LocationManagerProxy.getInstance(this);
+		mLocationManagerProxy.setGpsEnable(false);
+		// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+		// 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
+		// 在定位结束后，在合适的生命周期调用destroy()方法
+		// 其中如果间隔时间为-1，则定位只定一次,
+		// 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
+		mLocationManagerProxy.requestLocationData(
+				LocationProviderProxy.AMapNetwork, 60 * 1000, 15, this);
+
+	}
+	
+	@Override
+	public void onLocationChanged(Location location) {
+		
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		
+	}
+
+	@Override
+	public void onLocationChanged(AMapLocation amapLocation) {
+		if (amapLocation != null
+				&& amapLocation.getAMapException().getErrorCode() == 0) {
+			Log.i(TAG,"="+amapLocation.getLatitude() + "  "
+			+ amapLocation.getLongitude());
+		}else{
+			Log.e(TAG,"amapLocation==null");
+		}
+
 	}
 }
