e66b8e6d8590f58ab57f720c1abb93e19616f98e
zhaolu <zhaolu@jiuan.com>
2015-10-16 11:09:45 +0800

    添加了一个蓝色 箭头的位置指向

--- src/com/example/android_map_location/MainActivity.java
+++ src/com/example/android_map_location/MainActivity.java
@@ -4,17 +4,25 @@ import com.amap.api.location.AMapLocation;
 import com.amap.api.location.AMapLocationListener;
 import com.amap.api.location.LocationManagerProxy;
 import com.amap.api.location.LocationProviderProxy;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.LocationSource.OnLocationChangedListener;
 
 import android.app.Activity;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 
-public class MainActivity extends Activity implements AMapLocationListener{
+public class MainActivity extends Activity implements 
+	AMapLocationListener,LocationSource{
+	
 	private LocationManagerProxy mLocationManagerProxy;
 	private String TAG = "MainActivity";
-	private MapView mapView;
+	private OnLocationChangedListener mListener;
+    MapView mapView;
+	private AMap aMap;
+	private LocationManagerProxy mAMapLocationManager;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,10 @@ public class MainActivity extends Activity implements AMapLocationListener{
 		// 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
 		mLocationManagerProxy.requestLocationData(
 				LocationProviderProxy.AMapNetwork, 10 * 1000, 15, this);
+		if (aMap == null) {
+			aMap = mapView.getMap();
+			setUpMap();
+		}
 
 	}
 	
@@ -65,6 +77,11 @@ public class MainActivity extends Activity implements AMapLocationListener{
 	public void onLocationChanged(AMapLocation amapLocation) {
 		if (amapLocation != null
 				&& amapLocation.getAMapException().getErrorCode() == 0) {
+			if(mListener != null){
+				mListener.onLocationChanged(amapLocation);// 显示系统小蓝点
+			}else{
+				Log.i(TAG,"mListener==null");
+			}
 			Log.i(TAG,"经度="+amapLocation.getLatitude());
 			Log.i(TAG,"纬度="+amapLocation.getLongitude());
 			Log.i(TAG,"地点="+amapLocation.getAddress());
@@ -84,5 +101,37 @@ public class MainActivity extends Activity implements AMapLocationListener{
 	protected void onStart(){
 		super.onStart();
 		init();
+	}
+	
+	/**
+	 * 设置一些amap的属性
+	 */
+	private void setUpMap() {
+		aMap.setLocationSource(this);// 设置定位监听
+		aMap.getUiSettings().setMyLocationButtonEnabled(true);// 设置默认定位按钮是否显示
+		aMap.setMyLocationEnabled(true);// 设置为true表示显示定位层并可触发定位，false表示隐藏定位层并不可触发定位，默认是false
+		// 设置定位的类型为定位模式 ，可以由定位、跟随或地图根据面向方向旋转几种
+		aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+	}
+
+	//与显示的蓝色箭头相关
+	@Override
+	public void activate(OnLocationChangedListener listener) {
+		mListener = listener;
+		if (mAMapLocationManager == null) {
+			mAMapLocationManager = LocationManagerProxy.getInstance(this);
+			// 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+			// 注意设置合适的定位时间的间隔（最小间隔支持为2000ms），并且在合适时间调用removeUpdates()方法来取消定位请求
+			// 在定位结束后，在合适的生命周期调用destroy()方法
+			// 其中如果间隔时间为-1，则定位只定一次
+			// 在单次定位情况下，定位无论成功与否，都无需调用removeUpdates()方法移除请求，定位sdk内部会移除
+			mAMapLocationManager.requestLocationData(
+					LocationProviderProxy.AMapNetwork, 60 * 1000, 10, this);
+		}
+	}
+
+	@Override
+	public void deactivate() {
+		
 	}
 }
